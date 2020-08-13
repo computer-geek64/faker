b0affe2b50b2f557de9c65d70f49f3f501c1095c
Ashish D'Souza <sudouser512@gmail.com>
2020-08-12 21:23:10 -0400

    Added curl demo code for main.c

--- src/main.c
+++ src/main.c
@@ -1,37 +1,12 @@
 // main.c
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <curl/curl.h>
 #include "main.h"
-
-int sendGetRequest(char *url) {
-    CURL *curl;
-    CURLcode response;
-
-    curl_global_init(CURL_GLOBAL_DEFAULT);
-    curl = curl_easy_init();
-
-    if(!curl) {
-        exit(1);
-    }
-
-    curl_easy_setopt(curl, CURLOPT_URL, url);
-
-    response = curl_easy_perform(curl);
-
-    if(response != CURLE_OK) {
-        printf("HTTP Error %d\n", response);
-    }
-
-    curl_easy_cleanup(curl);
-    curl_global_cleanup();
-    return response;
-}
+#include <stdio.h>
+#include "curl_helper.c"
 
 int main() {
-    printf("CURLE_OK: %d\n", CURLE_OK);
-    int responseCode = sendGetRequest("http://ipecho.net/plain");
-    printf("Response code: %d\n", responseCode);
+    char *responseText = sendGetRequest("http://ipecho.net/plain");
+    printf("IP Address: %s\n", responseText);
+    free(responseText);
     return 0;
 }
